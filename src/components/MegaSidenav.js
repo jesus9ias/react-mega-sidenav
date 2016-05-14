314a00a81c7286e4d01701dffe115e136d228ad3
Jesús Escamilla <jesusescamilla@hotmail.com>
2016-05-14 18:04:19 -0500

    apply some props

--- src/components/MegaSidenav.js
+++ src/components/MegaSidenav.js
@@ -1,21 +1,51 @@
 import React, {Component} from 'react';
 import SidenavItem from './SidenavItem';
 
-export default class MegaSidenav extends Component{
-  render() {
-    return (
-      <div className="megaSidenav">
-        <div className="megaSidenav-itemList">
-          {
-            this.props.items.map((item, i) => {
-              return <SidenavItem key={i}>{item}</SidenavItem>
-            })
-          }
+class MegaSidenav extends Component{
+
+  close(){
+    this.props.close();
+  }
+
+  render(){
+    if(this.props.open == true){
+      return (
+        <div className="megaSidenav open">
+          <div className="megaSidenav-block">
+            <h2>{this.props.title}</h2>
+            {
+              this.props.useClose == true? <a onClick={this.close.bind(this)}>X</a> : null
+            }
+            <div className="megaSidenav-itemList">
+              {
+                this.props.items.map((item, i) => {
+                  return <SidenavItem key={i}>{item}</SidenavItem>
+                })
+              }
+            </div>
+            <div className="megaSidenav-content">
+              {this.props.children}
+            </div>
+          </div>
+          <div className="megaSidenav-backdrop"></div>
         </div>
-        <div className="megaSidenav-content">
-          {this.props.children}
-        </div>
-      </div>
-    );
+      );
+    }else{
+      return (<div className="megaSidenav close"></div>)
+    }
   }
 }
+
+MegaSidenav.defaultProps = {
+  open: true,
+  backdrop: true,
+  items: [],
+  fixed: true,
+  orientation: 'left',
+  useClose: true,
+  closeOnBackdrop: true,
+  title: '',
+  toggleIcon: '',
+}
+
+export default MegaSidenav
