0e962ddf376be3b8b78783e9806e81d2013cb877
Jesús Escamilla <jesusescamilla@hotmail.com>
2016-05-13 22:42:46 -0500

    setting items

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React, {Component} from 'react';
+import SidenavItem from './components/SidenavItem';
 
 export default class MegaSidenav extends Component{
   render() {
-    return (<div>Hi! megaSidenav</div>);
+    return (
+      <div clasName="megaSidenav">
+        <div>
+          {
+            this.props.items.map((item, i) => {
+              return <SidenavItem key={i}>{item}</SidenavItem>
+            })
+          }
+        </div>
+        <div>
+          {this.props.children}
+        </div>
+      </div>
+    );
   }
 }
