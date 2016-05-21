113c751eb47f7797dc62d0cbc2bd243f7d00a470
Jesús Escamilla <jesusescamilla@hotmail.com>
2016-05-20 22:11:36 -0500

    items styles

--- src/components/SidenavItem.js
+++ src/components/SidenavItem.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
 
+import InlineCss from 'react-inline-css'
+import itemStyles from '../styles/items'
+
 export default class SidenavItem extends Component{
   render() {
-    return (<div className="megaSidenav-item">{this.props.children}</div>);
+    return (
+      <InlineCss stylesheet={itemStyles()}>
+        <div className="megaSidenav-item">{this.props.children}</div>
+      </InlineCss>
+    );
   }
 }
---
+++ src/styles/items.js
@@ -0,0 +1,8 @@
+export default function itemStyles(){
+  return `& .megaSidenav-item {
+    width: calc(100% - 20px);
+    padding: 10px;
+    margin: 0px;
+    font-size: 16px;
+  }`;
+}
